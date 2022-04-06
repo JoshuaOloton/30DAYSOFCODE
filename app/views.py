3ac4fd4c919c03dfb73a848de838a005a19ef7f9
Joshua Oloton <olotonjoshua@gmail.com>
2022-04-06 23:33:49 +0100

    Day 2

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
+@app.route('/home')
 def home():
     name = 'Joshua Oloton'
     return render_template('index.html', name=name)
+
+@app.route('/home/<username>')
+def home_greeting(username):
+    return render_template('index.html', username=username)
