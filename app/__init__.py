cfef664b90e01ea85970cea2a87321dd85061c84
Joshua Oloton <olotonjoshua@gmail.com>
2022-04-07 22:53:29 +0100

    Day 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALchemy_TRACK_MODIFICATIONS'] = False
 
 from app import views
